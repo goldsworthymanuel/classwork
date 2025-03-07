ce8bf46b9a7b1cced0051d05d25f70bfb5456fd0
goldsworthymanuel <goldsworthymanuel@gmail.com>
2025-03-07 23:01:25 +0000

    Add 2.java

---
+++ src/2.java
@@ -0,0 +1,9 @@
+
+import java.util.*;
+
+public class RandomJavaCode {
+    public static void main(String[] args) {
+        // Write your code here
+        System.out.println("Hello, World!");
+    }
+}
