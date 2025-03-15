a2f3d16405e1780fd95070dea1cc3ce058e765c0
goldsworthymanuel <goldsworthymanuel@gmail.com>
2025-03-15 23:24:58 +0000

    Add 9.java

---
+++ src/9.java
@@ -0,0 +1,14 @@
+
+import java.util.*;
+public class Main {
+  public static void main(String[] args) {
+    System.out.println("Hello World!");
+    String name = "Alice";
+    int age = 30;
+    if (age > 25) {
+      System.out.println("You are over 25");
+    } else {
+      System.out.println("You are under 25");
+    }
+  }
+}
