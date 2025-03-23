c9a652229732f59a27cfe5f2d7c200962ae9fc77
goldsworthymanuel <goldsworthymanuel@gmail.com>
2025-03-23 00:04:10 +0000

    Add 17.java

---
+++ src/17.java
@@ -0,0 +1,14 @@
+public class MyClasswork {
+    public static void main(String[] args) {
+        // Example of simple Java program with basic logic
+
+        System.out.println("Hello, World!");
+        
+        int age = 20;
+        if (age >= 18) {
+            System.out.println("You are old enough to vote.");
+        } else {
+            System.out.println("Sorry, you are too young to vote.");
+        }
+    }
+}
