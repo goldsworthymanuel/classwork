03e110755388ee41bc9fa28d3be9b28d769cbbf4
goldsworthymanuel <goldsworthymanuel@gmail.com>
2025-04-08 18:42:35 +0000

    Add 27.java

---
+++ src/27.java
@@ -0,0 +1,6 @@
+public class MyClass {
+    public static void main(String[] args) {
+        // Example of simple Java program with basic print statement
+        System.out.println("Hello, world!");
+    }
+}
