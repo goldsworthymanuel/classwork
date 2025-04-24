6b7beaeffc3b6e467798e88b8eb6b35bc84046b6
goldsworthymanuel <goldsworthymanuel@gmail.com>
2025-04-24 15:08:38 +0000

    Add 30.java

---
+++ src/30.java
@@ -0,0 +1,12 @@
+public class MyClasswork {
+    public static void main(String[] args) {
+        // Example Java code to demonstrate basic functionality
+        int sum = 0;
+        for (int i = 1; i <= 5; i++) {
+            if (i % 2 == 0) {
+                sum += i;
+            }
+        }
+        System.out.println("The sum of even numbers between 1 and 5 is: " + sum);
+    }
+}
