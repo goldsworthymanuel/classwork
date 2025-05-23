bdc0f70ed87df0556cd5a6388af5bbe1cde7f325
goldsworthymanuel <goldsworthymanuel@gmail.com>
2025-05-23 08:02:11 +0000

    Add 37.java

---
+++ src/37.java
@@ -0,0 +1,7 @@
+public class MyClass {
+    public static void main(String[] args) {
+        // Randomly select and print an integer between 1 and 50
+        int number = (int)(Math.random() * 50) + 1;
+        System.out.println("The random number is: " + number);
+    }
+}
