c14299d2baef1f5768cd078fea02c1ea49434096
goldsworthymanuel <goldsworthymanuel@gmail.com>
2025-05-24 23:41:46 +0000

    Add 41.java

---
+++ src/41.java
@@ -0,0 +1,11 @@
+public class MyClass {
+    private String name;
+
+    public MyClass(String name) {
+        this.name = name;
+    }
+
+    public void displayInfo() {
+        System.out.println("Name: " + name);
+    }
+}
